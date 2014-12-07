1b83f74897a98c8c00acc0a9a6fa131e2057f93b
Robert Gwynn-Jones <gwynnr@gmail.com>
2014-12-07 12:58:07 +1100

    Clean up styles and html for index page, and add basic trudat functionality

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,10 @@
-console.log('This would be the main JS file.');
+$(document).ready(function () {
+    $("#trudat_question").blur(function() {
+        $("#trudat_answer").show();
+    });
+
+    $("#trudat_question").focus(function() {
+        $("#trudat_answer").hide();
+        $("#trudat_question").val("");
+    });
+});
