8aa589b4bcc743b83dd6a442aea58f1ac02646ad
Rob Gwynn-Jones <gwynnr@gmail.com>
2017-02-18 12:17:55 +1100

    Make the JS much more sensible

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,10 +1,25 @@
 $(document).ready(function () {
-    $("#trudat_question").blur(function() {
-        $("#trudat_answer").show();
-    });
+    var timeoutShowAnswer;
+    var timeoutExpireAnswer;
 
-    $("#trudat_question").focus(function() {
+    function showAnswer() {
+        var question = $("#trudat_question");
+        var input_length = question[0].value.length;
+
+        if (input_length > 0) {
+            $("#trudat_answer").show();
+            timeoutExpireAnswer = setTimeout(hideAnswer, 3000);
+        }
+    }
+
+    function hideAnswer() {
         $("#trudat_answer").hide();
-        $("#trudat_question").val("");
+    }
+
+    $("#trudat_question").on("input", function() {
+        clearTimeout(timeoutShowAnswer);
+        clearTimeout(timeoutExpireAnswer);
+        hideAnswer();
+        timeoutShowAnswer = setTimeout(showAnswer, 1000);
     });
 });
